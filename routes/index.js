56c7cd850f24fe936b455cba2b9a81c3a5120b40
Camille <Your email>
2023-05-15 15:23:29 +0200

    save schema et model

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,6 @@ var router = express.Router();
 require('../models/connection');
 const User = require('../models/users');
 
-router.post('/user', (req, res) => {
-  User.push(req.body.newUser)
-  res.json({userList: User})
-
-
-})
-
-router.get('/users', (req, res)=> {
-  res.json({userslist: User})
-})
 
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-const mongoose = require('mongoose');
 
 require('../models/connection');
 const User = require('../models/users');
 
-router.post('/user', (req, res) => {
-  User.push(req.body.newUser)
+router.post('/', (req, res) => {
+  // User.push(req.body.newUser)
   res.json({userList: User})
 
 
 })
 
-router.get('/users', (req, res)=> {
+router.get('/', (req, res)=> {
   res.json({usersList: User})
 });
 
 
 
 
-/* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
+
 
 module.exports = router;
