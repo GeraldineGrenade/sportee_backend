afc7981f7ba638722cf7821c77b78f0cf54581aa
Géraldine Grenade <geraldine.grenade@gmail.com>
2023-05-16 10:49:55 +0200

    fixing checkUsername route again

--- routes/users.js
+++ routes/users.js
@@ -23,7 +23,7 @@ router.get('/checkUsername/:username', (req, res) => {
 User.findOne({username : req.params.username})
   .then(data => {
     console.log('data in back ---', data)
-    data === undefined ? res.json({result : true}) : res.json({result : false})
+    data === null ? res.json({result : true}) : res.json({result : false})
   })
 });
 
