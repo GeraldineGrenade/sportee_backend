dcd5a929113ce6d36ed6eeb6aa4847efe9b0fa15
Géraldine Grenade <geraldine.grenade@gmail.com>
2023-05-16 10:08:12 +0200

    modified checkUsername route

--- routes/users.js
+++ routes/users.js
@@ -23,7 +23,7 @@ router.get('/checkUsername/:username', (req, res) => {
 User.findOne({username : req.params.username})
   .then(data => {
     console.log('data in back ---', data)
-    data === null ? res.json({result : true}) : res.json({result : false})
+    data === undefined ? res.json({result : true}) : res.json({result : false})
   })
 });
 
