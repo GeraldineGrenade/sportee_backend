6232ed5cca35cb2c07f7863dc5b9246afca024bc
Géraldine Grenade <geraldine.grenade@gmail.com>
2023-05-21 09:59:21 +0200

    modifying get activities route to only populate sport

--- routes/activities.js
+++ routes/activities.js
@@ -34,7 +34,7 @@ router.post ('/', async (req, res) => {
 router.get('/', async (req, res) => {
     try {
         const activities = await Activity.find()
-        .populate('user')
+        // .populate('user')
         .populate('sport')
         // .populate('participants.user')
         // .populate('conversation.users')
